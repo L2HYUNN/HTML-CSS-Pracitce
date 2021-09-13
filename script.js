dcf4128a0f7eaa95fea0ed13ea931c6033103e66
L2HYUNN <dhl9810@naver.com>
2021-09-13 23:11:19 +0900

    object can have array and annonymous function

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-var value = null;
-console.log(value);
-console.log(typeof(value));
-var value2;
-console.log(value2);
-console.log(typeof(value2));
-
-if(value == value2) {
-    console.log("value == value2")
-} else {
-    console.log("value !== value2")
+var object1 = {
+    arrays : [1, 2, 3]
 }
 
-if(value === value2) {
-    console.log("value === value2")
-} else {
-    console.log("value !=== value2")
+console.log(object1.arrays);
+
+const functionNameAdd = (n1, n2) => {
+    var sum = n1 + n2;
+    return sum;
 }
+
+console.log(functionNameAdd(3, 5))

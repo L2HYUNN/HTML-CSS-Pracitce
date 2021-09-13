f0e32675d7ca54d12d7196633703a9d8c3d52a9b
L2HYUNN <dhl9810@naver.com>
2021-09-13 21:18:04 +0900

    chapter 15

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var store = {snack: 1000, flower: 5000, beverage: 3000};
+var hometown = [
+    {name: '남준' , place: '일산' , city: '고양'},
+    {name: '진' , place: '과천'},
+    {name: '호석' , place: '광주' , city: '전라도'},
+    {name: '지민' , place: '부산' , city: '경상도'},
+];
 
-for (var item in store) {
-    if(!store.hasOwnProperty(item)) continue;
 
-    console.log(item + '는 가격이 ' + store[item] + ' 입니다.')
+
+var isHometown = function(h, name) {
+    console.log(`함수가 실행되었습니다. ${h.city} 도시에서 ${name} 을 찾습니다.`);
+
+    if(h.name === name) {
+        console.log(`${h.name}의 고향은 ${h.city} ${h.place} 입니다.`);
+        return true;
+    }
+
+    return false;
+}
+
+var h;
+while(h = hometown.shift()) {
+    if(!h.name || !h.city || !h.place) continue;
+
+    var result = isHometown(h, '호석');
+    if (result) break;
 }

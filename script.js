e0786df0c0cabaac0c31a7fefbab95bad79dff49
L2HYUNN <dhl9810@naver.com>
2021-09-13 21:28:39 +0900

    chapter 19

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-var hometown = [
-    {name: '남준' , place: '일산' , city: '고양'},
-    {name: '진' , place: '과천'},
-    {name: '호석' , place: '광주' , city: '전라도'},
-    {name: '지민' , place: '부산' , city: '경상도'},
-];
+var value = null;
+console.log(value);
+console.log(typeof(value));
+var value2;
+console.log(value2);
+console.log(typeof(value2));
 
-
-
-var isHometown = function(h, name) {
-    console.log(`함수가 실행되었습니다. ${h.city} 도시에서 ${name} 을 찾습니다.`);
-
-    if(h.name === name) {
-        console.log(`${h.name}의 고향은 ${h.city} ${h.place} 입니다.`);
-        return true;
-    }
-
-    return false;
+if(value == value2) {
+    console.log("value == value2")
+} else {
+    console.log("value !== value2")
 }
 
-var h;
-while(h = hometown.shift()) {
-    if(!h.name || !h.city || !h.place) continue;
-
-    var result = isHometown(h, '호석');
-    if (result) break;
+if(value === value2) {
+    console.log("value === value2")
+} else {
+    console.log("value !=== value2")
 }

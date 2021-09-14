daa2424c169156f87c31007c833e2bcedc6b5061
L2HYUNN <dhl9810@naver.com>
2021-09-14 13:48:48 +0900

    chapter 35-1

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function checkNumber(val) {
   if (typeof val !== 'number' ) throw '유효하지 않은 값이다.';
   console.log('숫자형 값으로 확인되었습니다.');
 }
-
-checkNumber(100);
-checkNumber('wrong type');
-console.log('완료');
+try{
+  checkNumber(100);
+  checkNumber('wrong type');
+} catch(e) {
+  console.log(`에러가 발상했습니다 > ${e}`);
+} finally {
+  console.log('완료');
+}

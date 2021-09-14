0840870211a9336b8db592af68275fa8b63f537d
L2HYUNN <dhl9810@naver.com>
2021-09-14 13:28:51 +0900

    chapter 34

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-const symbol = Symbol()
-const hello = Symbol('hello');
-console.log(symbol, hello);
-
-console.log('Number(3) === Number(3):');
-console.log(Number(3) === Number(3));
-
-console.log(Symbol('symbol') === Symbol('symbol'));
-console.log(Symbol() === Symbol());
-console.log(typeof Symbol());
-
-const nationility = Symbol('nationility');
-const user = {
-  name: 'jay'
-};
-user[nationility] = 'korean';
-console.log(user[nationility]);
-
-for (let key in user) {
-  console.log(key);
+var greeting_expression = function(name) {
+  console.log(`Hi ${name}`);
 }
 
-console.log(Object.keys(user));
-console.log(Object.getOwnPropertyNames(user));
-console.log(JSON.stringify(user));
+function greeting_declaration(name) {
+  console.log(`Hi ${name}`);
+}
 
-const symbolProperties = Object.getOwnPropertySymbols(user);
-console.log(symbolProperties);
-console.log(user[symbolProperties[0]]);
+greeting_expression('nico');
+greeting_declaration('nico');
+

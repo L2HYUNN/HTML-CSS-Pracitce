3630d12686e258dc71af6d26ba5aeb195b975a01
L2HYUNN <dhl9810@naver.com>
2021-09-14 12:43:31 +0900

    chapter 32

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-var address = 'Seoul';
-var members = {};
-var addFamily = function(age, name, role) {
-  this.members[role] = { age, name };
-};
-var getHeadcount = function() {
-  return Object.keys(this.members).length;
-};
+var obj = {a: 1, b: 10, c: 30, d: 50, e: 100};
 
-var family = { address, members, addFamily, getHeadcount };
+var {a, c} = obj;
+console.log(`a and c = ${a}, ${c}`);
 
-family.addFamily(30, 'chloe', 'aunt');
-family.addFamily(3, 'lyn', 'niece');
-family.addFamily(10, 'dangdangi', 'dog');
-console.log(family.getHeadcount());
+var {a: NewA=10, f: NewF=5000} = obj
+console.log(`newA and newF = ${NewA} and ${NewF}`)
+
+console.log(obj);
+
+var {NewA=100, f: NewF=5000} = obj
+console.log(`newA and newF = ${NewA} and ${NewF}`)
+
+// 배열의 비구조화 할당
+
+var arr = [1, 3, 30 ,50 ,25];
+
+var [b, c, ...rest] = arr;
+console.log(`b and c and rest = ${b}, ${c}, ${rest}`);
+
+var [a=10, f=20] = [1];
+console.log(`a and f = ${a}, ${f}`);
+
+[a, f] = [f, a];
+console.log(`a and f = ${a}, ${f}`); 
+
+function getArr() {
+  return [1, 2, 3, 4, 5, 6];  
+}
+[a, , , , ,f] = getArr();
+console.log(`a and f = ${a}, ${f}`); 

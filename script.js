29838aeaa155a927bcc201816dad35f2ac005489
L2HYUNN <dhl9810@naver.com>
2021-09-15 20:16:22 +0900

    chapter 47

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
-let user = {};
-Object.defineProperty(user, 'age', {
-  get: function() {
-    return this._age;
-  },
-  set: function(age) {
-    if ( age < 0 ) {
-      console.error('0보다 작은값은 올 수 없습니다.');
-    } else {
-      this._age = age;
-    }
-  },
-  enumerable: true
-});
-user.age = 10;
-console.log(user.age);
-user.age = -1;
-
-let user2 = {
-  get name() {
-    return this._name;
-  },
-  set name(val) {
-    if (val.length < 3) {
-      throw new Error('3자 이상이어야 합니다.'); 
-    } else {
-      this._name = val;
-    }
+const printArguments = (...args) => {
+  let total = 0;
+  for( let i = 0; i < args.length; i++) {
+    total += args[i];
   }
-}
-user2.name = 'harin';
-console.log(user2.name);
-user2.name = 'ge';
+  return total;
+} 
+let result = printArguments(1, 2, 3);
+
+console.log(result);
+
+function sum() {
+  let total = 0;
+ for( let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+} 
+let result2 = sum(1, 2, 3);
+
+console.log(result2);
+

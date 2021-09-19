9787c87c8e0074988c34eeab757870d595707092
L2HYUNN <dhl9810@naver.com>
2021-09-19 19:53:26 +0900

    chapter 125

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
+
+const product = [{name: '가방'}, {name: '노트북'}];
+
+for (const item of product) {
+  console.log(item);
+}
+
+const productIter = product[Symbol.iterator]();
+console.log(productIter.next());
+console.log(productIter.next());

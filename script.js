787a7222086777330fd1775f8f2e7e17475d8995
L2HYUNN <dhl9810@naver.com>
2021-09-19 12:06:48 +0900

    chapter 109

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const ship = {
-  max: 4,
-  passengers: [],
-  onBoard: function(name) {
-    if(this.passengers.length == this.max) {
-      console.log("This ship is full");
-      for(let i = 0; i < this.passengers.length; i++){
-        console.log(`${this.passengers[i]} is our passengers`);
-      }
-      return this.passengers
-    } else {
-      this.passengers.push(name);
-      console.log(`${name} on Board`);
-    }
-  }
+const result = {
+    floor: [],
+    ceil: [],
+    round: []
 }
-ship.onBoard('nico');
-ship.onBoard('jiaho');
-ship.onBoard('yoyo');
-ship.onBoard('nodoka');
-ship.onBoard('muyako');
+let max = 1000000000;
+for(let i = 0; i < max; i++) {
+  const rand = Math.random() * 10;
+  const f = Math.floor(rand);
+  const c = Math.ceil(rand);
+  const r = Math.round(rand);
+  
+  result.floor[f] = result.floor[f] + 1 || 1;
+  result.ceil[c] = result.ceil[c] + 1 || 1;
+  result.round[r] = result.round[r] + 1 || 1;
+}
+
+Object.keys(result).forEach(key => {
+  let data = result[key];
+  for(let i = 0; i < data.length; i ++){
+    data[i] = data[i] / max * 100 + '%';
+  }
+});
+console.log(result);
+
+

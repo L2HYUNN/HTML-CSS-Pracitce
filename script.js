df24f7a6b10bf6d4259e6a7327ba6bf0e24296b9
L2HYUNN <dhl9810@naver.com>
2021-09-19 18:01:17 +0900

    chapter 119

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-const testStringify = {
-  stringifiedNum: JSON.stringify(13.1),
-  stringifiedStr: JSON.stringify("Carnival"),
-  stringifiedBln: JSON.stringify(false),
-  stringifiedArr: JSON.stringify([2003, 2006]),
-};
+const str = "To lose your path is the way to find that path fuck you";
 
-for ( let key in testStringify) {
-  console.log(`@@@@@@@ ${key} @@@@@@@`);
-  console.log(typeof testStringify[key]);
-  console.log(testStringify[key]);
-}
+const sensitiveCaseRegex = /to/;
+const ignoreAllCaseRegex = /to/gi;
+const findRangeRegex = /([a-f])\w+/i;
+const findAllRangeRegex = /([a-f])\w+/gi;
 
-const obj = { 
-  drama: 'PET',
-  season: 2017,
-  casting: ['koyuki', 'matsumoto jun'],
-  character: ['sumire', 'momo']
-};
-
-const jsonObj = JSON.stringify(obj);
-console.log( typeof jsonObj );
-console.log( jsonObj );
-console.log(JSON.stringify(obj, ['drama', 'season']));
-console.log(JSON.stringify(obj, null, 4 )); 
-console.log(JSON.stringify(obj, (key, val) => {
-  if( val == 2017 ) return 2099;
-  return val; 
-}, 4));
+console.log(str.match(sensitiveCaseRegex));
+console.log(str.match(ignoreAllCaseRegex));
+console.log(str.match(findRangeRegex));
+console.log(str.match(findAllRangeRegex));
